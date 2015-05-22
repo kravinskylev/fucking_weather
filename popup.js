81a8e03780eb1fcc2eceb080a689bf84d49e00e8
Lev Kravinsky <kravinskylev@gmail.com>
2015-05-22 17:24:05 -0600

    version 0.2: needs styling and location

---
+++ popup.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($) {
+  $.ajax({
+  url : "https://api.wunderground.com/api/45bc33d3467bf2dc/geolookup/conditions/q/CO/Denver.json",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+  var city = parsed_json['location']['city'];
+  var state = parsed_json['location']['state'];
+  var weather = parsed_json['current_observation']['weather'];
+  var x = weather.toLowerCase();
+  var location = "&nbsp" + city + ", " + state
+
+      if (x.includes("cloud")) {
+     document.getElementById('para').innerHTML = "Cloudy with a chance of bullshit"
+  } else if (x.includes("overcast")) {
+     document.getElementById('para').innerHTML = "Fifty fucking shades of grey."
+  } else if (x.includes("rain")) {
+     document.getElementById('para').innerHTML = "Cloudy with a chance of bullshit"
+  } else if (x.includes("clear")) {
+     document.getElementById('para').innerHTML = "Suns out, get naked. Is that right?"
+  } else {
+     document.getElementById('para').innerHTML = "WTF is going on outside?"
+  }
+  document.getElementById('loc').innerHTML = location;
+
+  }
+  });
+});
